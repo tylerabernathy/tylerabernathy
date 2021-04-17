ff0ee9b25e78c58c4d48fb0584d4de2b3197b217
tylerabernathy <54758856+tylerabernathy@users.noreply.github.com>
2021-04-17 13:26:44 -0500

    Updated repository

---
+++ projects/subway-surfers-ad-autoclicker/main/main.py
@@ -0,0 +1,33 @@
+from time import *
+
+import PIL.ImageGrab
+from pynput.mouse import Button, Controller
+
+
+def main():
+    sleep(7)
+    rgb = PIL.ImageGrab.grab().load()[857, 579]
+    while rgb != (69, 157, 44):
+        rgb = PIL.ImageGrab.grab().load()[857, 579]
+    mouse.position = (1001, 766)
+    mouse.click(Button.left)
+    sleep(38)
+    mouse.position = (1780, 85)
+    mouse.click(Button.left)
+    sleep(1)
+    mouse.position = (1190, 55)
+    mouse.click(Button.left)
+    sleep(1)
+    mouse.position = (666, 58)
+    mouse.click(Button.left)
+    sleep(1)
+    mouse.position = (793, 995)
+    mouse.click(Button.left)
+    sleep(3)
+    mouse.position = (793, 995)
+    mouse.click(Button.left)
+
+
+mouse = Controller()
+while True:
+    main()
---
+++ projects/subway-surfers-ad-autoclicker/main/readme.md
@@ -0,0 +1,15 @@
+# Subway Surfers Auto Ad Clicker
+
+Subway Surfers Auto Ad Clicker is an auto clicker meant to obtain the user free rewards in Subway Surfers.
+
+## Requirements
+
+- 1920x1080 desktop resolution
+- Subway Surfers installed on BlueStacks
+
+## Usage
+
+1. Open the BlueStacks window in fullscreen
+2. Open the BlueStacks sidebar
+3. Navigate to Shop > Free Stuff in Subway Surfers
+4. Run the program (there will be a seven second delay after opening)
